00db3f075fabcec5d40ebe4e8870368e952b2953
enricosalazaratmb <enrico.salazar@anytimemailbox.com>
2025-12-10 15:52:44 +0800

    Modified migration service to include international sites

--- Services/MigrationService.cs
+++ Services/MigrationService.cs
@@ -8,6 +8,7 @@ using SiteLocationMigration.Models;
 using SiteLocationMigration.Models.Modern;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -62,7 +63,10 @@ namespace SiteLocationMigration.Services
 
             try
             {
-                await MigrateUsaSiteLocationsToGeographies();
+                await MigrateParentSiteLocationsToGeographies();
+                await MigrateChildrenSiteLocationsToGeographies();
+
+                // await MigrateUsaSiteLocationsToGeographies();
                 await SetGeoCityToNull();
                 await AssignGeographiesToLocations();
 
@@ -101,6 +105,191 @@ namespace SiteLocationMigration.Services
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"FAILURE: Could not update geographies to set GeoCity to null.");
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+        
+        /// <summary>
+        /// Gets parent sites first so that we have a reference
+        /// </summary>
+        /// <returns></returns>
+        private async Task MigrateParentSiteLocationsToGeographies()
+        {
+            // Get parent locations first since they are root
+            var parentSiteLocations = (await _legacyAtmbContext.SiteLocations.ToListAsync()).Where(sl => sl.DefaultText != string.Empty && sl.ParentSitLocId == 0).ToList();
+            var existingExternalKeys = await _modernAtmbContext.Geographies.Select(g => g.ExternalKey).Where(k => k != null).Distinct().ToListAsync();
+            var existingKeysSet = new HashSet<string>(existingExternalKeys, StringComparer.OrdinalIgnoreCase);
+            var geographies = new List<ENT_Geography>();
+
+            var index = 1;
+            foreach (var parentSiteLocation in parentSiteLocations)
+            {
+                Console.WriteLine($"Location {index}/{parentSiteLocations.Count} being migrated: ${parentSiteLocation.DefaultText}, SitLocId: {parentSiteLocation.SitLocId}, ParentSitLocId: {parentSiteLocation.ParentSitLocId}");
+                var regionInfo = FindRegionByCountryName(parentSiteLocation.DefaultText);
+
+                // No need to insert duplicates
+                if (existingKeysSet.Contains(parentSiteLocation.WebKey))
+                {
+                    Console.WriteLine($"Location '{parentSiteLocation.DefaultText}' already exists in the DB as '{parentSiteLocation.WebKey}'");
+                    index++;
+                    continue;
+                }
+
+                var geographyEquivalent = new ENT_Geography()
+                {
+                    ParentId = Guid.Empty,
+                    ExternalKey = parentSiteLocation.WebKey,
+                    GeoCity = null, // Set to null per request
+                    GeoCountryName = parentSiteLocation.DefaultText,
+                    GeoCountryISO = regionInfo?.ThreeLetterISORegionName ?? "",
+                    GeoLatitude = parentSiteLocation.MapLat,
+                    GeoLongitude = parentSiteLocation.MapLong,
+                    GeoRegionISO = regionInfo?.TwoLetterISORegionName ?? "",
+                    GeoRegionName = null, // Parent's GeoRegionName is always NULL
+                    MapZoom = parentSiteLocation.GoogleZoom,
+                    Name = parentSiteLocation.DefaultText,
+                    SourceKey = string.Empty, // Remains empty
+                    Status = "ACTIVE",
+                    Type = "PUBLIC",
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                };
+
+                geographies.Add(geographyEquivalent);
+                index++;
+            }
+
+            try
+            {
+                await _modernAtmbContext.Geographies.AddRangeAsync(geographies);
+                int recordsAffected = await _modernAtmbContext.SaveChangesAsync();
+
+                Console.WriteLine($"======================== MigrateParentSiteLocationsToGeographies ========================");
+                Console.WriteLine($"INSERTED in ENT_Geography table. Rows affected: {recordsAffected}");
+                Console.WriteLine($"================================================");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"FAILURE: Could not add new geography.");
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Gets parent sites and links them to child sites
+        /// </summary>
+        /// <returns></returns>
+        private async Task MigrateChildrenSiteLocationsToGeographies()
+        {
+            // Get parent locations first since they are root
+            var siteLocations = (await _legacyAtmbContext.SiteLocations.ToListAsync()).Where(sl => sl.DefaultText != string.Empty && sl.ParentSitLocId != 0).ToList();
+            var existingExternalKeys = await _modernAtmbContext.Geographies.Select(g => g.ExternalKey).Where(k => k != null).Distinct().ToListAsync();
+            var existingKeysSet = new HashSet<string>(existingExternalKeys, StringComparer.OrdinalIgnoreCase);
+            var geographies = new List<ENT_Geography>();
+
+            var index = 1;
+            foreach (var siteLocation in siteLocations)
+            {
+                var parentLocation = (await _legacyAtmbContext.SiteLocations.ToListAsync()).Where(sl => sl.SitLocId == siteLocation.ParentSitLocId).FirstOrDefault();
+                
+                if (parentLocation == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("==============================");
+                    Console.WriteLine($"FAILURE: This site location {siteLocation.SitLocId} - {siteLocation.DefaultText} does not have an existing parent with SitLocId of '{siteLocation.ParentSitLocId}'.");
+                    Console.WriteLine("==============================");
+                    Console.ResetColor();
+                    index++;
+                    continue;
+                }
+
+                var parentGeography = (await _modernAtmbContext.Geographies.ToListAsync()).Where(g => g.ExternalKey == parentLocation.WebKey).FirstOrDefault();
+
+                if (parentGeography == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("==============================");
+                    Console.WriteLine($"FAILURE: This site location's parent geography does not exist in ENT_Geography for parent WebKey '{parentLocation.WebKey}'.");
+                    Console.WriteLine("==============================");
+                    Console.ResetColor();
+                    index++;
+                    continue;
+                }
+
+                Console.WriteLine($"Location {index}/{siteLocations.Count} being migrated: ${siteLocation.DefaultText}, SitLocId: {siteLocation.SitLocId}, ParentSitLocId: {siteLocation.ParentSitLocId}");
+                var regionInfo = FindRegionByCountryName(siteLocation.DefaultText);
+
+                // No need to insert duplicates
+                if (existingKeysSet.Contains(siteLocation.WebKey))
+                {
+                    Console.WriteLine($"Location '{siteLocation.DefaultText}' already exists in the DB as '{siteLocation.WebKey}'. Updating instead");
+
+                    var existingGeography = (await _modernAtmbContext.Geographies.ToListAsync()).Where(g => g.ExternalKey == siteLocation.WebKey).FirstOrDefault();
+                    existingGeography.GeoCountryISO = regionInfo?.ThreeLetterISORegionName ?? "";
+                    existingGeography.GeoRegionISO = regionInfo?.TwoLetterISORegionName ?? "";
+                    existingGeography.Name = siteLocation.DefaultText;
+                    existingGeography.GeoCountryName = siteLocation.DefaultText;
+
+                    try
+                    {
+                        int recordsAffected = await _modernAtmbContext.SaveChangesAsync();
+
+                        Console.WriteLine($"======================== MigrateChildrenSiteLocationsToGeographies ========================");
+                        Console.WriteLine($"UPDATED in ENT_Geography table. Rows affected: {recordsAffected}");
+                        Console.WriteLine($"================================================");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"FAILURE: Could not update new geography.");
+                        Console.WriteLine($"Error: {ex.Message}");
+                        Console.ResetColor();
+                    }
+
+                    index++;
+                    continue;
+                }
+
+                var geographyEquivalent = new ENT_Geography()
+                {
+                    ParentId = parentGeography.GeographyId,
+                    ExternalKey = siteLocation.WebKey,
+                    GeoCity = null, // Set to null per request
+                    GeoCountryName = siteLocation.DefaultText,
+                    GeoCountryISO = regionInfo?.ThreeLetterISORegionName ?? "",
+                    GeoLatitude = siteLocation.MapLat,
+                    GeoLongitude = siteLocation.MapLong,
+                    GeoRegionISO = regionInfo?.TwoLetterISORegionName ?? "",
+                    GeoRegionName = siteLocation.DefaultText,
+                    MapZoom = siteLocation.GoogleZoom,
+                    Name = siteLocation.DefaultText,
+                    SourceKey = string.Empty, // Remains empty
+                    Status = "ACTIVE",
+                    Type = "PUBLIC",
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                };
+
+                geographies.Add(geographyEquivalent);
+                index++;
+            }
+
+            try
+            {
+                await _modernAtmbContext.Geographies.AddRangeAsync(geographies);
+                int recordsAffected = await _modernAtmbContext.SaveChangesAsync();
+
+                Console.WriteLine($"======================== MigrateChildrenSiteLocationsToGeographies ========================");
+                Console.WriteLine($"INSERTED in ENT_Geography table. Rows affected: {recordsAffected}");
+                Console.WriteLine($"================================================");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"FAILURE: Could not add new geography.");
                 Console.WriteLine($"Error: {ex.Message}");
                 Console.ResetColor();
             }
@@ -182,13 +371,48 @@ namespace SiteLocationMigration.Services
             }
         }
 
+        private RegionInfo FindRegionByCountryName(string countryName)
+        {
+            if (string.IsNullOrWhiteSpace(countryName))
+                return null;
+
+            // Normalize the search name
+            string normalizedName = countryName.Trim();
+
+            try
+            {
+                RegionInfo region = CultureInfo
+                .GetCultures(CultureTypes.SpecificCultures)
+                .Select(c =>
+                {
+                    try { return new RegionInfo(c.Name); }
+                    catch { return null; }
+                })
+                .Where(r => r != null)
+                .FirstOrDefault(r =>
+                    r.EnglishName.Equals(countryName, StringComparison.OrdinalIgnoreCase));
+
+                return region;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"FAILURE: Could not get list of regions.");
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ResetColor();
+            }
+
+            return null;
+        }
+
         private async Task AssignGeographiesToLocations()
         {
             // Set to 2000 per request
             const int MAX_RECORDS_TO_ADD = 5;
+            const bool IS_INTERNATIONAL = true;
 
             var preexistingGeographies = (await _modernAtmbContext.Geographies.ToListAsync());
-            var preexistingLocations = (await _modernAtmbContext.Locations.ToListAsync());
+            var preexistingLocations = IS_INTERNATIONAL ? (await _modernAtmbContext.Locations.Where(l => l.GeoCountryISO != "USA").ToListAsync()) : (await _modernAtmbContext.Locations.ToListAsync());
             var preexistingGeographyLocations = (await _modernAtmbContext.GeographyLocations.ToListAsync());
 
             int locationInsertedCount = 0;
